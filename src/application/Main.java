de50e5240f18ee5ab3464be8a16845d691d17cc7
Indy <Indy@DESKTOP-48D8FC0>
2016-12-11 00:06:47 +0700

    Backup #5

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,13 @@
 package application;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import logic.GameManager;
 import ui.GameScreen;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 
 public class Main extends Application {
@@ -15,8 +18,36 @@ public class Main extends Application {
 		GameScreen gsc = new GameScreen(800,600);
 		GameManager gm = new GameManager();
 		root.getChildren().add(gsc);
-		gsc.paintComponents();
-		
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
+
+			@Override
+			public void handle(KeyEvent event) {
+				// TODO Auto-generated method stub
+				gm.receiveKey(event.getCode());
+			}
+		});
+		scene.setOnKeyReleased(new EventHandler<KeyEvent>(){
+
+			@Override
+			public void handle(KeyEvent event) {
+				// TODO Auto-generated method stub
+				gm.dropKey(event.getCode());
+			}
+		});
+		new AnimationTimer() {
+			Long start=0l;
+			@Override
+			public void handle(long now) {
+				// TODO Auto-generated method stub
+				if(start==0l)start=now;
+				long diff = now-start;
+				if(diff>=100000000l){ //100000000l = 100ms.
+					gm.update();
+					gsc.paintComponents();
+					start = now;
+				}
+			}
+		}.start();
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Bobo Game");
 		primaryStage.show();
--- src/logic/GameManager.java
+++ src/logic/GameManager.java
@@ -7,17 +7,19 @@ import model.IRenderable;
 import model.RenderableHolder;
 import javafx.scene.input.KeyCode;
 import model.*;
+
 public class GameManager {
-	private int timer=0;
-	public static int hp=100;
+	private int timer = 0;
+	public static int hp = 100;
 	private PlayerChar player;
 	private BossChar boss;
-	public GameManager(){
-		player = new PlayerChar(150,475,0);
-		boss = new BossChar(550,400);
+
+	public GameManager() {
+		player = new PlayerChar(150, 475, 0);
+		boss = new BossChar(550, 400);
 		RenderableHolder.getInstance().add(player);
 		RenderableHolder.getInstance().add(boss);
-		
+
 	}
 
 	private void addEntity(IRenderable entity) {
@@ -25,22 +27,22 @@ public class GameManager {
 		RenderableHolder.getInstance().add(entity);
 	}
 
-	public int getTimer(){
+	public int getTimer() {
 		return timer;
 	}
-	
+
 	public void update() {
 		// TODO Auto-generated method stub
-		//Fill in here
+		// Fill in here
 		timer++;
 		move();
 		checkCollision();
 		removeDestroyEntity();
-		if(timer%20 == 0 && !player.isDestroy()){
+		if (timer % 20 == 0 && !player.isDestroy()) {
 			Random rand = new Random();
 			int xx = rand.nextInt(470);
 			int yy = rand.nextInt(470);
-			Bullet bullet = new Bullet(boss.getX()-10,boss.getY());
+			Bullet bullet = new Bullet(boss.getX() - 10, boss.getY());
 			addEntity(bullet);
 		}
 	}
@@ -54,63 +56,70 @@ public class GameManager {
 		}
 	}
 
-	private boolean isCollide(PlayerChar player, Entity e){
-		return checkX(player,e) && checkY(player,e);
+	private boolean isCollide(PlayerChar player, Entity e) {
+		return checkX(player, e) && checkY(player, e);
 	}
-	private boolean checkX(PlayerChar player, Entity e){
-		return (player.getX()+player.getWidth()>e.getX() && player.getX() < e.getX()) ||
-				(e.getX() + e.getWidth() > player.getX() && e.getX() < player.getX());
+
+	private boolean checkX(PlayerChar player, Entity e) {
+		return (player.getX() + player.getWidth() > e.getX() && player.getX() < e.getX())
+				|| (e.getX() + e.getWidth() > player.getX() && e.getX() < player.getX());
 	}
-	private boolean checkY(PlayerChar player, Entity e){
-		return (player.getY()+player.getHeight()>e.getY() && player.getY() < e.getY()) ||
-				(e.getY() + e.getHeight() > player.getY() && e.getY() < player.getY());
+
+	private boolean checkY(PlayerChar player, Entity e) {
+		return (player.getY() + player.getHeight() > e.getY() && player.getY() < e.getY())
+				|| (e.getY() + e.getHeight() > player.getY() && e.getY() < player.getY());
 	}
-	
+
 	private void checkCollision() {
 		// TODO Auto-generated method stub
-		//Body head = Snake.body.get(0);
-		//Fill in here
-		for(IRenderable i : RenderableHolder.getInstance().getEntities()){
-			if(i instanceof Bullet){
-				if(isCollide(player,(Bullet)i)){
-					((Bullet)i).setDestroy();
+		// Body head = Snake.body.get(0);
+		// Fill in here
+		for (IRenderable i : RenderableHolder.getInstance().getEntities()) {
+			if (i instanceof Bullet) {
+				if (isCollide(player, (Bullet) i)) {
+					((Bullet) i).setDestroy();
 					player.decreaseLife();
 				}
 			}
 		}
-		
-	}
-	
-	/*private void checkDead(){
-		if(arrow.getX()<0 || arrow.getX()>451 || arrow.getY()<0 || arrow.getY()>451){
-			arrow.isDestroy = true;
-		}
-		
-	}*/
 
-	private void move() {
-		// TODO Auto-generated method stub
-		//walk according to received key
 	}
+
+	/*
+	 * private void checkDead(){ if(arrow.getX()<0 || arrow.getX()>451 ||
+	 * arrow.getY()<0 || arrow.getY()>451){ arrow.isDestroy = true; }
+	 * 
+	 * }
+	 */
+
+	 private void move() {
+	 // TODO Auto-generated method stub
+	 //walk according to received key
+		 if(!CodeUtility.keyPressed.isEmpty()){
+			 if(CodeUtility.keyPressed.contains(KeyCode.A)){
+				 player.setX(player.getX()-10);
+			 }
+			 if(CodeUtility.keyPressed.contains(KeyCode.D)){
+				 player.setX(player.getX()+10);
+			 }
+			 if(CodeUtility.keyPressed.contains(KeyCode.W)){
+				 player.setY(player.getY()-15);
+			 }
+		 }
+		 else if(player.getY()+15<=475){
+			 player.setY(player.getY()+15);
+		 }
+	 }
 
 	public void receiveKey(KeyCode new_code) {
 		// TODO Auto-generated method stub
 		if (!CodeUtility.keyPressed.contains(new_code)) {
 			CodeUtility.keyPressed.add(new_code);
 			CodeUtility.keyTriggered.add(new_code);
-			
-			if(new_code == KeyCode.A){
-				//walk left
-				player.setX(player.getX()-10);
-				}
-			else if(new_code == KeyCode.D){
-				//walk right
-				player.setX(player.getX()+10);
-				}
+
+			move();
 		}
 	}
-	
-
 
 	public void dropKey(KeyCode new_code) {
 		// TODO Auto-generated method stub
