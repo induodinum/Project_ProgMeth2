2f4b93937f6117e19c4905201f1c3ffeb5f2987c
Indy <Indy@DESKTOP-48D8FC0>
2016-12-10 22:30:18 +0700

    Backup #1

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,26 +2,18 @@ package application;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import ui.GameScreen;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.StackPane;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		GridPane root = new GridPane();
+		StackPane root = new StackPane();
 		Scene scene = new Scene(root, 800, 600);
-		Canvas canvas = new Canvas(800,600);		
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-		root.add(canvas, 0, 0);
-		
-		Image brick = new Image(ClassLoader.getSystemResource("brick.png").toString(),50 ,50 ,false ,false);
-		gc.setFill(Color.LIGHTGRAY);
-		gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-		gc.drawImage(brick, 0, 0);
+		GameScreen gsc = new GameScreen(800,600);
+		root.getChildren().add(gsc);
+		gsc.paintComponents();
 		
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Bobo Game");
--- src/ui/GameScreen.java
+++ src/ui/GameScreen.java
@@ -2,6 +2,7 @@ package ui;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -10,35 +11,43 @@ import model.IRenderable;
 import model.RenderableHolder;
 import model.Entity;
 
-public class GameScreen extends Canvas{
+public class GameScreen extends Canvas {
 	public static int screen_width, screen_height;
-	
-	public GameScreen(int width,int height){
-		super(width,height);
+
+	public GameScreen(int width, int height) {
+		super(width, height);
 		screen_height = height;
 		screen_width = width;
 	}
-	
-	public void paintComponents(){
+
+	public void paintComponents() {
 		GraphicsContext gc = this.getGraphicsContext2D();
-		gc.setFill(Color.BLACK);
+		gc.setFill(Color.LIGHTGRAY);
 		gc.fillRect(0, 0, screen_width, screen_height);
-		for(IRenderable i : RenderableHolder.getInstance().getEntities()){
-			if(!i.isDestroy())
-				i.draw(gc);
+		Image brick = new Image(ClassLoader.getSystemResource("brick.png").toString(), 50, 50, false, false);
+		for (int i = 0; i < screen_width; i += 50) {
+			for (int j = 0; j < screen_height; j += 50) {
+				if (j == 0 || j == 550 || i == 0 || i == 750) {
+					gc.drawImage(brick, i, j);
+				}
+			}
 		}
-		drawScore(gc);
-		
+//		for(IRenderable i : RenderableHolder.getInstance().getEntities()){
+//			if (!i.isDestroy())
+//				i.draw(gc);
+//		}
+//		drawScore(gc);
+
 	}
-	
-	public void drawScore(GraphicsContext gc){
+
+	public void drawScore(GraphicsContext gc) {
 		int sc = GameManager.score;
 		String st = "";
-		if(sc<100 && sc >=10){
+		if (sc < 100 && sc >= 10) {
 			st = "0" + sc;
-		}else if(sc<10 && sc >=0){
+		} else if (sc < 10 && sc >= 0) {
 			st = "00" + sc;
-		}else{
+		} else {
 			st = Integer.toString(sc);
 		}
 		Font f = Font.font("Times New Roman", 50);
@@ -46,7 +55,7 @@ public class GameScreen extends Canvas{
 		gc.setFill(Color.BLUE);
 		gc.setStroke(Color.WHITE);
 		gc.strokeText(st, 350, 50);
-		
+
 		gc.fillText(st, 350, 50);
 	}
 }
